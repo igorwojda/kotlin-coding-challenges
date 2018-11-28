11ac275b9c6f453a07202af946ee0d8038649790
igor.wojda <igor.wojda@gmail.com>
2018-11-28 16:15:52 +0000

    added recursive palindrome

--- app/src/test/java/com/igorwojda/codingpuzzle/PalindromeSolution.kt
+++ app/src/test/java/com/igorwojda/codingpuzzle/PalindromeSolution.kt
@@ -1,9 +1,11 @@
 package com.igorwojda.codingpuzzle
 
+//string reverse
 private fun palindromeSolution1(str: String): Boolean {
     return str == str.reversed()
 }
 
+//iterative, double pointer solution
 private fun palindromeSolution2(str: String): Boolean {
     str.forEachIndexed { index, char ->
         val rightIndex = str.length - index - 1
@@ -16,6 +18,19 @@ private fun palindromeSolution2(str: String): Boolean {
     }
 
     return true
+}
+
+//recursive solution
+private fun palindrome(str: String): Boolean {
+    return if(str.isEmpty() || str.length == 1) {
+        true
+    } else {
+        if(str.first() == str.last()) {
+            palindrome(str.substring(1 until str.lastIndex))
+        } else {
+            false
+        }
+    }
 }
 
 //not working, because kotlin does not have allIndexed - equivalent of Array.prototype.every()
