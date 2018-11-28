1d5900a7a392f8c1546d312870ebc514b143f136
igor.wojda <igor.wojda@gmail.com>
2018-11-28 17:09:10 +0000

    added recursive tolerant palindrome

--- app/src/test/java/com/igorwojda/codingpuzzle/PalindromeWithToleranceSolution.kt
+++ app/src/test/java/com/igorwojda/codingpuzzle/PalindromeWithToleranceSolution.kt
@@ -1,6 +1,6 @@
 package com.igorwojda.codingpuzzle
 
-
+//iterative solution
 private fun tolerantPalindromeSolution1(str: String): Boolean {
     var characterRemoved = false
 
@@ -25,4 +25,30 @@ private fun tolerantPalindromeSolution1(str: String): Boolean {
     }
 
     return false
+}
+
+//recursive solution
+private fun tolerantPalindromeSolution2(str: String, characterRemoved: Boolean = false): Boolean {
+    return if(str.isEmpty() || str.length == 1) {
+        true
+    } else {
+        if(str.first() == str.last()) {
+            tolerantPalindromeSolution2(str.substring(1 until str.lastIndex), characterRemoved)
+        }
+        else {
+            if(characterRemoved) {
+                false
+            } else {
+                if (str.length == 2) {
+                    return true
+                }
+
+                println(str)
+                val removeLeftResult = tolerantPalindromeSolution2(str.substring(2 until str.lastIndex), true)
+                val removeRightResult = tolerantPalindromeSolution2(str.substring(1 until str.lastIndex - 1), true)
+
+                return removeLeftResult || removeRightResult
+            }
+        }
+    }
 }
