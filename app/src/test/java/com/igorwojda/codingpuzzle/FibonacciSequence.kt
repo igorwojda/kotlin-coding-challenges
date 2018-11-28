5c9162e6ff8f0d37e673b421f35251436d6ade52
igor.wojda <igor.wojda@gmail.com>
2018-11-28 18:45:37 +0000

    updated fibonacci solution

--- app/src/test/java/com/igorwojda/codingpuzzle/FibonacciSequence.kt
+++ app/src/test/java/com/igorwojda/codingpuzzle/FibonacciSequence.kt
@@ -13,12 +13,32 @@ import org.junit.Test
 // Example:
 //   fibonacciSequence(4) == 3
 
-
 private fun fibonacciSequence(n: Int): Int {
-    return 0
+    if(n == 0) {
+        return 0
+    } else if (n == 1) {
+        return 1
+    }
+
+    var prevPrev = 0
+    var prev = 1
+    var current = 0
+
+    (2..n).forEach {
+        current = prevPrev + prev
+        prevPrev = prev
+        prev = current
+    }
+
+    return current
 }
 
 class FibonacciSeriesTest {
+    @Test
+    fun `calculates correct fib value for 0`() {
+        fibonacciSequence(0) shouldEqual 0
+    }
+
     @Test
     fun `calculates correct fib value for 1`() {
         fibonacciSequence(1) shouldEqual 1
--- app/src/test/java/com/igorwojda/codingpuzzle/FibonacciSequenceSolution.kt
+++ app/src/test/java/com/igorwojda/codingpuzzle/FibonacciSequenceSolution.kt
@@ -1,7 +1,28 @@
 package com.igorwojda.codingpuzzle
 
 //iterative solution
-private fun fibonacciSequenceSolution(n: Int): Int {
+private fun fibonacciSequenceSolution1(n: Int): Int {
+    if(n == 0) {
+        return 0
+    } else if (n == 1) {
+        return 1
+    }
+
+    var prevPrev = 0
+    var prev = 1
+    var current = 0
+
+    (2..n).forEach {
+        current = prevPrev + prev
+        prevPrev = prev
+        prev = current
+    }
+
+    return current
+}
+
+//iterative solution with temporary list (not efficient)
+private fun fibonacciSequenceSolution2(n: Int): Int {
     val list = mutableListOf(0, 1)
 
     for (it in list.size..n) {
@@ -9,4 +30,22 @@ private fun fibonacciSequenceSolution(n: Int): Int {
     }
 
     return list[n]
+}
+
+//recursive solution
+private fun fibonacciSequenceSolution3(n: Int): Int {
+    return when (n) {
+        0 -> 0
+        1 -> 1
+        else -> fibonacciSequenceSolution3(n - 1) + fibonacciSequenceSolution3(n - 2)
+    }
+}
+
+//recursive solution
+private fun fibonacciSequence4(n: Int): Int {
+    if (n == 1 || n == 2) {
+        return 1
+    }
+
+    return fibonacciSequence4(n - 1) + fibonacciSequence4(n - 2)
 }
