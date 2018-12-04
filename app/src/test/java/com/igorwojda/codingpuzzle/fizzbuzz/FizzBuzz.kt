4df06c034f1728189979f3aaccbc77902bb7be74
igor.wojda <igor.wojda@gmail.com>
2018-12-04 23:53:51 +0000

    added fizzbuzz desc

--- app/src/test/java/com/igorwojda/codingpuzzle/fizzbuzz/FizzBuzz.kt
+++ app/src/test/java/com/igorwojda/codingpuzzle/fizzbuzz/FizzBuzz.kt
@@ -3,16 +3,6 @@ package com.igorwojda.codingpuzzle.fizzbuzz
 import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
-// --- Instructions
-// Write a program that adds to list numbers
-// from 1 to n. But for multiples of three print
-// “fizz” instead of the number and for the multiples
-// of five print “buzz”. For numbers which are multiples
-// of both three and five print “fizzbuzz”.
-// --- Example
-//   fizzBuzz(5)
-//   listOf(1, 2, "fizz", 4, "buzz")
-
 private fun fizzBuzz(n: Int): List<String> {
     return listOf()
 }
