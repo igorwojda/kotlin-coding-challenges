2b98b1fc0bf7d576d9fee567aee6d04cd8ae6b9d
igor.wojda <igor.wojda@gmail.com>
2018-12-05 15:58:40 +0000

    Updated fizzbuzz

--- app/src/test/java/com/igorwojda/codingpuzzle/fizzbuzz/FizzBuzzSolution.kt
+++ app/src/test/java/com/igorwojda/codingpuzzle/fizzbuzz/FizzBuzzSolution.kt
@@ -6,9 +6,9 @@ private object FizzBuzzSolution {
 
         (1..n).forEach {
             val item = when {
-                it % 3 == 0 && it % 5 == 0 -> "fizzbuzz" //also (it % 15 == 0) because 3x5=15
-                it % 3 == 0 -> "fizz"
-                it % 5 == 0 -> "buzz"
+                it % 3 == 0 && it % 5 == 0 -> "FizzBuzz" //also (it % 15 == 0) because 3x5=15
+                it % 3 == 0 -> "Fizz"
+                it % 5 == 0 -> "Buzz"
                 else -> it.toString()
             }
 
