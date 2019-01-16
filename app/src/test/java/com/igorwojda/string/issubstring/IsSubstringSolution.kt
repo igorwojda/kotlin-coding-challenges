67fae6fd582863c53be498cdb739b3e3eea96fa2
igor.wojda <igor.wojda@gmail.com>
2019-01-16 07:15:47 +0000

    Added IsSubstring

---
+++ app/src/test/java/com/igorwojda/string/issubstring/IsSubstringSolution.kt
@@ -0,0 +1,49 @@
+package com.igorwojda.string.issubstring
+
+// Time complexity: O(n+m)
+// Space complexity: O(1)
+// Double pointer solution
+private object OptimalSolution1 {
+    private fun isSubstring(first: String, second: String): Boolean {
+        if (first.isEmpty() && second.isEmpty()) return true
+        if (first.length <= second.length) return false
+
+        var i = 0
+        var j = 0
+
+        while (i <= first.lastIndex) {
+            if (first[i] == second[j]) {
+                j++
+            }
+
+            if (j == second.length) {
+                return true
+            }
+
+            i++
+        }
+
+        return false
+    }
+}
+
+// Time complexity: O(n+m)
+// Space complexity: ??? (definitely more than O(1))
+// Recursive solution
+private object Solution {
+    private fun isSubstring(first: String, second: String): Boolean {
+        if(second.length > first.length) {
+            return false
+        }
+
+        if(second.isEmpty()) {
+            return true
+        }
+
+        return if(first.first() == second.first()) {
+            isSubstring(first.drop(1), second.drop(1))
+        } else {
+            isSubstring(first.drop(1), second)
+        }
+    }
+}
