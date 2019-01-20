b89d5fb3ae237089a3e952642988c1a3bcb1283c
igor.wojda <igor.wojda@gmail.com>
2019-01-20 12:43:19 +0000

    Simplified count up and down solution Added 2nd count up and down solution

--- app/src/test/java/com/igorwojda/integer/countupanddown/CountUpAndDown.kt
+++ app/src/test/java/com/igorwojda/integer/countupanddown/CountUpAndDown.kt
@@ -7,6 +7,7 @@ private fun countUpAndDown(n: Int): List<Int> {
     return emptyList()
 }
 
+
 class CountUpAndDownTest {
     @Test
     fun `count up and down 0`() {
@@ -24,7 +25,17 @@ class CountUpAndDownTest {
     }
 
     @Test
+    fun `count up and down 3`() {
+        countUpAndDown(3) shouldEqual listOf(0, 1, 2, 3, 2, 1, 0)
+    }
+
+    @Test
     fun `count up and down 4`() {
         countUpAndDown(4) shouldEqual listOf(0, 1, 2, 3, 4, 3, 2, 1, 0)
+    }
+
+    @Test
+    fun `count up and down 9`() {
+        countUpAndDown(9) shouldEqual listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
     }
 }
