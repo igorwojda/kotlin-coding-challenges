2b6c433c449f8fc0bdb6da127194c86edbf48d45
igor.wojda <igor.wojda@gmail.com>
2019-01-20 17:33:34 +0000

    Added new solution to IsSubstring

--- app/src/test/java/com/igorwojda/string/issubstring/IsSubstringSolution.kt
+++ app/src/test/java/com/igorwojda/string/issubstring/IsSubstringSolution.kt
@@ -2,6 +2,7 @@ package com.igorwojda.string.issubstring
 
 // Time complexity: O(n+m)
 // Space complexity: O(1)
+//
 // Double pointer solution
 private object OptimalSolution1 {
     private fun isSubstring(first: String, second: String): Boolean {
@@ -28,11 +29,13 @@ private object OptimalSolution1 {
 }
 
 // Time complexity: O(n+m)
-// Space complexity: O(n*n)
-// number of iterations is bounded by the length of the first string
-// and String#drop requires copying the entire remaining string
+// Space complexity: ??, but more than O(1)
+// Number of iterations (n) is bounded by the length of the first string
+// and String.drop requires copying the entire remaining string (on it's own it has O(m) complexity)
+// String of 5 chars, needs 5 iterations at most and 15 character copied (5+4+3+2+1=15)
+//
 // Recursive solution
-private object Solution {
+private object RecursiveSolution1 {
     private fun isSubstring(first: String, second: String): Boolean {
         if (second.length > first.length) {
             return false
@@ -47,5 +50,46 @@ private object Solution {
         } else {
             isSubstring(first.drop(1), second)
         }
+    }
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+// This recursive solution is faster than solution with String.drop because it uses double pointer
+//
+// Recursive solution
+
+private object RecursiveSolution2 {
+    private fun isSubstring(first: String, second: String): Boolean {
+        if (second.isEmpty()) {
+            return true
+        }
+
+        fun helper(first: String, second: String, firstPointer1: Int = 0, secondPointer2: Int = 0): Boolean {
+            if (firstPointer1 > first.lastIndex) {
+                return false
+            }
+
+            return if (first[firstPointer1] == second[secondPointer2]) {
+                val p1 = firstPointer1 + 1
+                val p2 = secondPointer2 + 1
+
+                if (p1 > first.lastIndex || p2 > second.lastIndex) {
+                    return true
+                } else {
+                    helper(first, second, p1, p2)
+                }
+            } else {
+                val p1 = firstPointer1 + 1
+
+                if (p1 > first.lastIndex) {
+                    return false
+                } else {
+                    helper(first, second, p1, secondPointer2)
+                }
+            }
+        }
+
+        return helper(first, second)
     }
 }
