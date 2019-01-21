7b21c8636b45de17859a00c1faed221295f297a6
IgorWojda <igor.wojda@gmail.com>
2019-01-21 04:39:11 +0000

    capitalizeSentence clean up

--- app/src/test/java/com/igorwojda/string/capitalizesentence/CapitalizeSentence.kt
+++ app/src/test/java/com/igorwojda/string/capitalizesentence/CapitalizeSentence.kt
@@ -3,18 +3,18 @@ package com.igorwojda.string.capitalizesentence
 import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
-private fun capitalize(str: String): String {
+private fun capitalizeSentence(str: String): String {
     return ""
 }
 
 class CapitalizeSentenceTest {
     @Test
     fun `'flower' is capitalized to 'Flower'`() {
-        capitalize("flower") shouldEqual "Flower"
+        capitalizeSentence("flower") shouldEqual "Flower"
     }
 
     @Test
     fun `'this is a house' is capitalised to 'This Is A House'`() {
-        capitalize("this is a house") shouldEqual "This Is A House"
+        capitalizeSentence("this is a house") shouldEqual "This Is A House"
     }
 }
