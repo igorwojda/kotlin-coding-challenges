aa3d183bf3a6d2ad0e85c12b7824e66b327c36d2
igor.wojda <igor.wojda@gmail.com>
2019-01-23 07:38:02 +0000

    Added subtract

---
+++ app/src/test/java/com/igorwojda/list/subtract/SubtractSolution.kt
@@ -0,0 +1,17 @@
+package com.igorwojda.list.subtract
+
+private object Solution1 {
+    private fun getSubtraction(list1: List<String>, list2: List<String>): List<String> {
+        val frequencyMap1 = list1.groupingBy { it }.eachCount()
+        val frequencyMap2 = list2.groupingBy { it }.eachCount()
+
+        val itemList1 = frequencyMap1.flatMap { entry -> List(entry.value) { entry.key } }.toMutableList()
+        val itemList2 = frequencyMap2.flatMap { entry -> List(entry.value) { entry.key } }
+
+        itemList2.forEach { itemList1.remove(it) }
+
+        return itemList1
+    }
+}
+
+object KtLintWillNotComplain
