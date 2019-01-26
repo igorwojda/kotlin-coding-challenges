c30fe8cd3468753c27809eb67839ea6a74ada014
Igor Wojda <igor.wojda@gmail.com>
2019-01-26 16:02:44 +0000

    Update binary clean up

--- app/src/test/java/com/igorwojda/list/binarysearch/BinarySearch.kt
+++ app/src/test/java/com/igorwojda/list/binarysearch/BinarySearch.kt
@@ -5,25 +5,6 @@ import org.junit.Test
 
 
 private fun binarySearch(list: List<Char>, element: Char): Int {
-    var left = 0
-    var right = list.size - 1
-
-    while (left <= right) {
-        val middle = (right + left) / 2
-
-        println("L: $left (${list[left]}), M: $middle (${list[middle]}), R: $right (${list[right]})")
-        // Check if x is present at mid
-        if (list[middle] == element)
-            return middle
-
-        if (list[middle] < element)
-            left = middle + 1 // x greater than middle, so ignore left half
-        else
-            right = middle - 1 // x greater than middle, so , ignore right half
-    }
-
-    // if we reach here, then element was
-    // not present
     return -1
 }
 
