d692eadb8d5b3080748561437b084fb1461c708a
igor.wojda <igor.wojda@gmail.com>
2019-01-26 15:38:31 +0000

    Update binary search solution

--- app/src/test/java/com/igorwojda/list/binarysearch/BinarySearchSolution.kt
+++ app/src/test/java/com/igorwojda/list/binarysearch/BinarySearchSolution.kt
@@ -8,6 +8,7 @@ private object Solution {
     private fun binarySearch(list: List<Char>, element: Char): Int {
         var left = 0
         var right = list.size - 1
+
         while (left <= right) {
             val middle = (right + left) / 2
 
@@ -15,11 +16,10 @@ private object Solution {
             if (list[middle] == element)
                 return middle
 
-            // If x greater, ignore left half
             if (list[middle] < element)
-                left = middle + 1
+                left = middle + 1 // x greater than middle, so ignore left half
             else
-                right = middle - 1// If x is smaller, ignore right half
+                right = middle - 1 // x greater than middle, so , ignore right half
         }
 
         // if we reach here, then element was
