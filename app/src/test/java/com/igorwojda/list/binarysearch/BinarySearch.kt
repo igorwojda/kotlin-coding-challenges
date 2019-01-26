945f7768c5177d7a3a5d3508b9365a91f8364458
igor.wojda <igor.wojda@gmail.com>
2019-01-26 15:32:38 +0000

    Update binary search solution

--- app/src/test/java/com/igorwojda/list/binarysearch/BinarySearch.kt
+++ app/src/test/java/com/igorwojda/list/binarysearch/BinarySearch.kt
@@ -2,29 +2,12 @@ package com.igorwojda.list.binarysearch
 
 import org.amshove.kluent.shouldEqual
 import org.junit.Test
+import android.R.attr.x
+
+
 
 private fun binarySearch(list: List<Char>, element: Char): Int {
-    fun getMiddle(left: Int, right: Int) = Math.floor((right + left).toDouble() / 2).toInt()
-
-    var left = 0
-    var right = list.lastIndex
-    var middle = getMiddle(left, right)
-
-    while (list[middle] != element && left <= right) {
-        if (element < list[middle]) {
-            right = middle - 1
-        } else {
-            left = middle + 1
-        }
-
-        middle = getMiddle(left, right)
-    }
-
-    return if (list[middle] == element) {
-        middle
-    } else {
-        -1
-    }
+    return -1
 }
 
 class BinarySearchTest {
