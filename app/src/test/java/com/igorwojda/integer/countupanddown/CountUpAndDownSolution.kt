99fc04957ea785be2e40d2e70055040e31cf2cde
IgorWojda <igor.wojda@gmail.com>
2019-01-30 23:35:05 +0000

    Simplified countUpAndDown solutions

--- app/src/test/java/com/igorwojda/integer/countupanddown/CountUpAndDownSolution.kt
+++ app/src/test/java/com/igorwojda/integer/countupanddown/CountUpAndDownSolution.kt
@@ -1,21 +1,7 @@
 package com.igorwojda.integer.countupanddown
 
-// Create two lists and add them
-private object Solution1 {
-    private fun countUpAndDown(n: Int): List<Int> {
-        // Create first list containing integers from 0 to n
-        val upList = (0..n).toMutableList()
-
-        // Create second list containing integers from n -1 to 0
-        val downList = ((n - 1) downTo 0).toList()
-
-        // Join lists
-        return upList + downList
-    }
-}
-
 // Calculate element value from element position
-private object Solution2 {
+private object Solution1 {
     private fun countUpAndDown(n: Int): List<Int> {
         val size = (n * 2) + 1
 
@@ -28,14 +14,12 @@ private object Solution2 {
     }
 }
 
-// Returns a new list containing all the elements of an iterable plus the elements of another
-private object Solucion3 {
+// Returns a new list by combining two iterables
+private object Solution2 {
     private fun countUpAndDown(n: Int): List<Int> {
         // (0 until n) returns a range from 0 to (n - 1)
         // (n downTo 0) returns a range from n to 0
-        // "+" operator is the same as doing upIntRange.plus(downIntRange),
-        // which returns a list containing all elements of the original collection
-        // and then all elements of the given elements collection.
+        // "+" operator combines both collections
         return (0 until n) + (n downTo 0)
     }
 }
