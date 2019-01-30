e6b06e08fcb00947979b1f2e74282cbe8afbc338
Yannis <johnbarbakadze@gmail.com>
2019-01-30 13:32:06 +0000

    Update Int to String so that the test will not fail

--- app/src/test/java/com/igorwojda/various/fizzbuzz/FizzBuzz.kt
+++ app/src/test/java/com/igorwojda/various/fizzbuzz/FizzBuzz.kt
@@ -20,7 +20,7 @@ class FizzBuzzTest {
         val list = listOf(
             "1", "2", "Fizz", "4", "Buzz", "Fizz",
             "7", "8", "Fizz", "Buzz", "11", "Fizz",
-            "13", "14", "FizzBuzz", 16
+            "13", "14", "FizzBuzz", "16"
         )
 
         fizzBuzz(16) shouldEqual list
