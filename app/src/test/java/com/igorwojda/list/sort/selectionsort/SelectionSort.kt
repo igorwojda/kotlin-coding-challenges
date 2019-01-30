7797a10b48b24c832ac8617bda06eb117de521f3
Igor Wojda <igor.wojda@gmail.com>
2019-01-30 18:16:34 +0000

    updated test names

--- app/src/test/java/com/igorwojda/list/sort/selectionsort/SelectionSort.kt
+++ app/src/test/java/com/igorwojda/list/sort/selectionsort/SelectionSort.kt
@@ -9,12 +9,12 @@ private fun selectionSort(list: List<Int>): List<Number> {
 
 class SelectionSortTest {
     @Test
-    fun `insertion sort 5, 1, 4, 2`() {
+    fun `selection sort 5, 1, 4, 2`() {
         selectionSort(mutableListOf(5, 1, 4, 2)) shouldEqual listOf(1, 2, 4, 5)
     }
 
     @Test
-    fun `insertion sort 17, 4, 12, 19, 80, 75, 16`() {
+    fun `selection sort 17, 4, 12, 19, 80, 75, 16`() {
         selectionSort(mutableListOf(17, 4, 12, 19, 80, 75, 16)) shouldEqual listOf(4, 12, 16, 17, 19, 75, 80)
     }
 }
