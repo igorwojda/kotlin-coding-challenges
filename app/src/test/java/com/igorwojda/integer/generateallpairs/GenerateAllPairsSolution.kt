369727108f7cede8ddb0eab7cca41765a19aa2dd
David Mozano <davidmozano@gmail.com>
2019-01-30 23:18:33 +0100

    Generate all pairs - Added a new solution

--- app/src/test/java/com/igorwojda/integer/generateallpairs/GenerateAllPairsSolution.kt
+++ app/src/test/java/com/igorwojda/integer/generateallpairs/GenerateAllPairsSolution.kt
@@ -1,6 +1,6 @@
 package com.igorwojda.integer.generateallpairs
 
-private object Solution {
+private object Solution1 {
     private fun getAllPairs(n: Int): List<Pair<Int, Int>> {
         val result = mutableListOf<Pair<Int, Int>>()
 
@@ -11,6 +11,16 @@ private object Solution {
         }
 
         return result
+    }
+}
+
+private object Solution2 {
+    private fun getAllPairs(n: Int): List<Pair<Int, Int>> {
+        return (0..n).map { i ->
+            (0..n).map { j ->
+                i to j
+            }
+        }.flatten()
     }
 }
 
