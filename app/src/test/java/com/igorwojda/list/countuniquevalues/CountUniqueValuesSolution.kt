59dbbc89376f389e55fd6648affea7aeab44e5ae
igor.wojda <igor.wojda@gmail.com>
2019-02-19 23:12:54 +0000

    Updated CountUniqueValues solution

--- app/src/test/java/com/igorwojda/list/countuniquevalues/CountUniqueValuesSolution.kt
+++ app/src/test/java/com/igorwojda/list/countuniquevalues/CountUniqueValuesSolution.kt
@@ -14,20 +14,6 @@ private object Solution2 {
 
 private object Solution3 {
     private fun countUniqueValues(list: List<Int>): Int {
-        if (list.isEmpty()) return 0
-        if (list.size == 1) return 1
-
-        var count = 1
-        var prevElement = list[0]
-
-        (0..list.lastIndex).forEach {
-            if (prevElement != list[it]) {
-                count++
-            }
-
-            prevElement = list[it]
-        }
-
-        return count
+        return list.groupBy { it }.size
     }
 }
