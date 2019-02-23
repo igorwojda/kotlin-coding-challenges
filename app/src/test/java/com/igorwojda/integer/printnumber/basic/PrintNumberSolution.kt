3d94f8af3e4de57ff26a5fe5d6a3101ff79e7df4
David Mozano <davidmozano@gmail.com>
2019-02-23 19:12:06 +0100

    Print numbers - Added a new solution

--- app/src/test/java/com/igorwojda/integer/printnumber/basic/PrintNumberSolution.kt
+++ app/src/test/java/com/igorwojda/integer/printnumber/basic/PrintNumberSolution.kt
@@ -1,6 +1,12 @@
 package com.igorwojda.integer.printnumber.basic
 
-private object Solution {
+private object Solution1 {
+    private fun printNumber(n: Int): List<Int> {
+        return (n downTo 1).toList()
+    }
+}
+
+private object Solution2 {
     private fun printNumber(n: Int): List<Int> {
         val list = mutableListOf<Int>()
 
