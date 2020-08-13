84201833df722ca5aea9853ddc0842c8c200427f
Mr. Shabinder Singh <mr.shabinder@outlook.com>
2020-08-14 00:38:13 +0530

    Adding both approaches in decapitalizeConst.kt

--- app/src/test/java/com/igorwojda/string/decapitalizeconst/DecapitalizeConstSolution.kt
+++ app/src/test/java/com/igorwojda/string/decapitalizeconst/DecapitalizeConstSolution.kt
@@ -1,11 +1,29 @@
 package com.igorwojda.string.decapitalizeconst
 
 // Kotlin idiomatic solution
-private object Solution {
+private object Solution1 {
   private fun decapitalizeConst(str: String): String {
     val subsStringsList = str.split("_").map { it.toLowerCase().capitalize() }
     return subsStringsList.joinToString("").decapitalize()
   }
 }
+
+// Another Approach
+private object Solution2 {
+    private fun decapitalizeConst(str: String): String? {
+        val words = str.split("_").filter { it.isNotEmpty() }
+
+        if (words.size <= 1) return null
+
+        return words.mapIndexed { index, word ->
+            if (index == 0) {
+                word.toLowerCase()
+            } else {
+                word.first().toUpperCase() + word.drop(1).toLowerCase()
+            }
+        }.joinToString(separator = "")
+    }
+}
+
 
 private object KtLintWillNotComplain
