f04a882122cb4a8ca49142414ec6841eae53042f
Mr. Shabinder Singh <mr.shabinder@outlook.com>
2020-08-13 19:13:03 +0530

    Update Solution in DecapitalizeConstSolution.kt
    Added more Kotlin Idiomatic Approach.(Handles Edge Case Tests Too)

--- app/src/test/java/com/igorwojda/string/decapitalizeconst/DecapitalizeConstSolution.kt
+++ app/src/test/java/com/igorwojda/string/decapitalizeconst/DecapitalizeConstSolution.kt
@@ -2,19 +2,10 @@ package com.igorwojda.string.decapitalizeconst
 
 // Kotlin idiomatic solution
 private object Solution {
-    private fun decapitalizeConst(str: String): String? {
-        val words = str.split("_").filter { it.isNotEmpty() }
-
-        if (words.size <= 1) return null
-
-        return words.mapIndexed { index, word ->
-            if (index == 0) {
-                word.toLowerCase()
-            } else {
-                word.first().toUpperCase() + word.drop(1).toLowerCase()
-            }
-        }.joinToString(separator = "")
-    }
+  private fun decapitalizeConst(str: String): String {
+    val subsStringsList = str.split("_").map { it.toLowerCase().capitalize() }
+    return subsStringsList.joinToString("").decapitalize()
+  }
 }
 
 private object KtLintWillNotComplain
