a5e2134b9c9e7c1fd8f685b24ff78d4f39f5f173
Igor <igor.wojda@gmail.com>
2021-03-01 21:54:44 +0100

    Add solution

--- app/src/test/java/com/igorwojda/integer/getodd/GetOddSolution.kt
+++ app/src/test/java/com/igorwojda/integer/getodd/GetOddSolution.kt
@@ -10,6 +10,22 @@ private object Solution1 {
 // Recursive solution
 private object Solution2 {
     private fun filterOdd(list: List<Int>): List<Int> {
+
+        if (list.isEmpty()) {
+            return list
+        }
+
+        return if (list.first() % 2 == 1) {
+            mutableListOf(list.first()) + filterOdd(list.drop(1))
+        } else {
+            filterOdd(list.drop(1))
+        }
+    }
+}
+
+// Recursive solution with helper function
+private object Solution3 {
+    private fun filterOdd(list: List<Int>): List<Int> {
         val result = mutableListOf<Int>()
 
         fun helper(list: List<Int>) {
