a9f3c9370e57d55e40e470bd300afae70c82a50c
Igor <igor.wojda@gmail.com>
2021-03-01 21:36:18 +0100

    Add new solution

--- app/src/test/java/com/igorwojda/integer/countdown/CountDownSolution.kt
+++ app/src/test/java/com/igorwojda/integer/countdown/CountDownSolution.kt
@@ -11,6 +11,17 @@ private object Solution {
 // Recursive solution
 private object Solution2 {
     private fun countDown(n: Int): List<Int> {
+        if (n == 0) {
+            return listOf(0)
+        }
+
+        return mutableListOf(n).also { it.addAll(countDown(n - 1)) }
+    }
+}
+
+// Recursive solution with helper function
+private object Solution3 {
+    private fun countDown(n: Int): List<Int> {
         // We want to keep return type unchanged while implementing recursive solution, so we will
         // use helper method defied inside countDown function.
         fun helper(n: Int): MutableList<Int> {
