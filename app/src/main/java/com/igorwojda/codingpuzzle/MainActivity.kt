975e69d93401df69aad7fba4a21ceb19a7b5b424
Igor <igor.wojda@gmail.com>
2021-03-01 17:09:09 +0100

    Updated dependencies

--- app/src/main/java/com/igorwojda/codingpuzzle/MainActivity.kt
+++ app/src/main/java/com/igorwojda/codingpuzzle/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.igorwojda.codingpuzzle
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
