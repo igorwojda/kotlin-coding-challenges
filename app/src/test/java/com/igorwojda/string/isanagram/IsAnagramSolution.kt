147b496eb041f58565f0020ad8d8e2f6a512e28a
Igor <igor.wojda@gmail.com>
2021-03-02 23:36:03 +0100

    Add solution

--- app/src/test/java/com/igorwojda/string/isanagram/IsAnagramSolution.kt
+++ app/src/test/java/com/igorwojda/string/isanagram/IsAnagramSolution.kt
@@ -1,6 +1,14 @@
 package com.igorwojda.string.isanagram
 
-private object Solution {
+private object Solution1 {
+    private fun isAnagram(str1: String, str2: String): Boolean {
+        val a1 = str1.toUpperCase().filter { it.isLetter() }.groupBy { it }
+        val a2 = str2.toUpperCase().filter { it.isLetter() }.groupBy { it }
+        return a1 == a2
+    }
+}
+
+private object Solution2 {
     private fun isAnagram(str1: String, str2: String): Boolean {
         return getCharFrequency(str1) == getCharFrequency(str2)
     }
