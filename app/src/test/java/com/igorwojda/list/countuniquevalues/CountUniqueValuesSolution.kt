2cfe75c18a54f755cad1644f3cead723da2584ff
Igor <igor.wojda@gmail.com>
2021-03-02 23:25:16 +0100

    Add solution

--- app/src/test/java/com/igorwojda/list/countuniquevalues/CountUniqueValuesSolution.kt
+++ app/src/test/java/com/igorwojda/list/countuniquevalues/CountUniqueValuesSolution.kt
@@ -2,17 +2,31 @@ package com.igorwojda.list.countuniquevalues
 
 private object Solution1 {
     private fun countUniqueValues(list: List<Int>): Int {
-        return list.toSet().size
+        val map = mutableMapOf<Int, Int>()
+
+        list.forEach {
+            var value = map.getOrDefault(it, 0)
+            value++
+            map[it] = value
+        }
+
+        return map.count()
     }
 }
 
 private object Solution2 {
     private fun countUniqueValues(list: List<Int>): Int {
-        return list.distinct().size
+        return list.toSet().size
     }
 }
 
 private object Solution3 {
+    private fun countUniqueValues(list: List<Int>): Int {
+        return list.distinct().size
+    }
+}
+
+private object Solution4 {
     private fun countUniqueValues(list: List<Int>): Int {
         return list.groupBy { it }.size
     }
