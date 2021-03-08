19c50ddb72da6599642f1add858ccae565266fd9
Igor <igor.wojda@gmail.com>
2021-03-08 14:35:20 +0100

    Add new solution

--- app/src/test/java/com/igorwojda/string/reverse/ReverseSolution.kt
+++ app/src/test/java/com/igorwojda/string/reverse/ReverseSolution.kt
@@ -19,17 +19,42 @@ private object Solution2 {
 }
 
 // Recursive approach
-private fun reverse(str: String): String {
-    if (str.isEmpty()) {
-        return str
-    }
+private object Solution3 {
+    private fun reverse(str: String): String {
+        if (str.isEmpty()) {
+            return str
+        }
 
-    return reverse(str.drop(1)) + str.first()
+        return reverse(str.drop(1)) + str.first()
+    }
 }
 
 // Kotlin fold
-private object Solution3 {
+private object Solution4 {
     private fun reverse(str: String): String {
         return str.foldRight("") { char, reversed -> reversed + char }
+    }
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+// Reverse in place
+private object Solution5 {
+    private fun reverse(str: String): String {
+        val chars = str.toMutableList()
+
+        var leftIndex = 0
+        var rightIndex = chars.lastIndex
+
+        while (leftIndex <= rightIndex) {
+            val temp = chars[leftIndex]
+            chars[leftIndex] = chars[rightIndex]
+            chars[rightIndex] = temp
+
+            leftIndex++
+            rightIndex--
+        }
+
+        return chars.joinToString(transform = { it.toString() }, separator = "")
     }
 }
