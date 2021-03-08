cfeab22130af5b5a8f0d21f0e3968e0df53e40ec
Igor <igor.wojda@gmail.com>
2021-03-08 18:07:11 +0100

    Add solution

--- app/src/test/java/com/igorwojda/string/hasrepeatedcharacter/HasRepeatedCharSolution.kt
+++ app/src/test/java/com/igorwojda/string/hasrepeatedcharacter/HasRepeatedCharSolution.kt
@@ -1,7 +1,15 @@
 package com.igorwojda.string.hasrepeatedcharacter
 
 // Time complexity: O(n)
-private object Solution {
+private object Solution1 {
+    private fun hasRepeatedChar(str: String): Boolean {
+        val frequency = str.groupingBy { it }.eachCount()
+        return frequency.any { it.value > 1 }
+    }
+}
+
+// Time complexity: O(n)
+private object Solution2 {
     private fun hasRepeatedChar(str: String): Boolean {
         val frequency = str.groupingBy { it }.eachCount()
 
@@ -14,5 +22,3 @@ private object Solution {
         return false
     }
 }
-
-private object KtLintWillNotComplain
