e60b55be32efdd3c9b4b71deff076a34d11667b9
Igor <igor.wojda@gmail.com>
2021-03-10 00:16:51 +0100

    Upd template

--- misc/template/challenge/challenge.kt
+++ misc/template/challenge/challenge.kt
@@ -3,13 +3,14 @@ package com.igorwojda.puzzle
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
-private fun puzzle() {
+private fun challenge() {
 
 }
 
 private class Test {
     @Test
     fun `simple test`() {
-        "result" shouldBeEqualTo "expected"
+        val result = challenge()
+        result shouldBeEqualTo "expected"
     }
 }
