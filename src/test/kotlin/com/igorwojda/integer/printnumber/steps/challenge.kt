3cec9b44bccb7024fdd6f7946fc8bc15fd10b40d
Catalin Gruia <cataliingruia@gmail.com>
2021-11-10 21:13:12 +0200

    First implementation of Print numbers with steps
    First working implementation of Print numbers with steps

--- src/test/kotlin/com/igorwojda/integer/printnumber/steps/challenge.kt
+++ src/test/kotlin/com/igorwojda/integer/printnumber/steps/challenge.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun printNumber(n: Int, step: Int = 1): List<Int> {
-    TODO("not implemented")
+    return (n downTo 1 step step).toList()
 }
 
 class RecursivePrintNumberWithSteps {
