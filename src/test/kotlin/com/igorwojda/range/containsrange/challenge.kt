5d6bb9c0facbf7ae0a88969ef324430e1090f4e7
Catalin Gruia <cataliingruia@gmail.com>
2021-11-10 14:53:07 +0200

    Working implementation of Range contains range?
    First working implementation of Range contains range?

--- src/test/kotlin/com/igorwojda/range/containsrange/challenge.kt
+++ src/test/kotlin/com/igorwojda/range/containsrange/challenge.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun containsRange(range1: IntRange, range2: IntRange): Boolean {
-    TODO("not implemented")
+    return (range2.first >= range1.first) && (range2.last <= range1.last)
 }
 
 private class Test {
