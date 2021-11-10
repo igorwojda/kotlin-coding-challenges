7e51e8bed818895a7e8cac362d9eab987791800f
Catalin Gruia <cataliingruia@gmail.com>
2021-11-10 22:00:10 +0200

    First implementation of Product
    First working implementation of Product

--- src/test/kotlin/com/igorwojda/list/product/challenge.kt
+++ src/test/kotlin/com/igorwojda/list/product/challenge.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun product(list: List<Int>): Int {
-    TODO("not implemented")
+    return list.reduce { accumulator, current -> accumulator * current }
 }
 
 private class Test {
