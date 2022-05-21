511714e483d11bba5522867df173cf21327fb82b
Kaloyan Karaivanov <skarloti@gmail.com>
2022-05-21 17:12:48 +0300

    Update Kotlin 1.6.21 & Coroutines 1.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.4.31"
+	kotlin("jvm") version "1.6.21"
 }
 
 repositories {
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
 	testImplementation("org.amshove.kluent:kluent:1.65")
 }
