37826a7f663725cfd85679e02c8870c1dd86885e
Igor Wojda <igor.wojda@gmail.com>
2022-08-25 22:22:08 +0200

    Upd dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.4.31"
+	kotlin("jvm") version "1.7.10"
 }
 
 repositories {
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-	testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
-	testImplementation("org.amshove.kluent:kluent:1.65")
+	testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+	testImplementation("org.amshove.kluent:kluent:1.68")
 }
 
 sourceSets {
