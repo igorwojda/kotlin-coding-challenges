235403fb1874bcae7216e07c891076a7325a0979
Igor Wojda <igor.wojda@gmail.com>
2022-09-15 19:24:52 +0200

    Add com.adarshr.test-logger to main project

--- .test/buildSrc/build.gradle.kts
+++ .test/buildSrc/build.gradle.kts
@@ -1,16 +1,5 @@
-buildscript {
-    repositories {
-        maven("https://plugins.gradle.org/m2/")
-    }
-
-    dependencies {
-        classpath("com.adarshr:gradle-test-logger-plugin:3.2.0")
-    }
-}
-
 plugins {
     `kotlin-dsl`
-    id("com.adarshr.test-logger") version "3.2.0"
 }
 
 // The kotlin-dsl plugin requires a repository to be declared
