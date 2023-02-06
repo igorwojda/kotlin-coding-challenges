cb6bf84fcdbc20f0d0b8c41841d3f2527bb3ca90
natalia.peterwas <n.peterwas@gmail.com>
2023-02-06 11:48:00 +0100

    Added TODO("not implemented")

--- src/test/kotlin/com/igorwojda/string/issubstring/challenge.kt
+++ src/test/kotlin/com/igorwojda/string/issubstring/challenge.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun isSubstring(str: String, subStr: String): Boolean {
-    TODO()
+    TODO("not implemented")
 }
 
 private class Test {
