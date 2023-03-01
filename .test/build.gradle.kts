f691c46b5e8c939a634f39be7eceb98fa854496e
Igor Wojda <igor.wojda@gmail.com>
2023-03-01 23:42:13 +0100

    Add Spotless test project

--- .test/build.gradle.kts
+++ .test/build.gradle.kts
@@ -5,7 +5,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 plugins {
     kotlin("jvm") version "1.8.10"
     id("com.adarshr.test-logger") version "3.2.0"
-    id("org.jlleitschuh.gradle.ktlint") version "11.2.0"
+    id("com.diffplug.spotless") version "6.15.0"
 }
 
 repositories {
@@ -38,3 +38,13 @@ task("generateTests") {
 kotlin {
     jvmToolchain(17)
 }
+
+spotless {
+    kotlin {
+        ktlint()
+
+        indentWithSpaces()
+        endWithNewline()
+    }
+}
+
