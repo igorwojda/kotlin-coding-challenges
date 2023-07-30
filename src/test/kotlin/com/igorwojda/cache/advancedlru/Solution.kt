977c99e753118b4137d83badbd1f8031314791dd
Jayson Minard <jayson.minard@gmail.com>
2023-07-30 15:22:03 -0600

    fixes for AdvancedLRU with naive implementtion

--- src/test/kotlin/com/igorwojda/cache/advancedlru/Solution.kt
+++ src/test/kotlin/com/igorwojda/cache/advancedlru/Solution.kt
@@ -1,35 +1,46 @@
 package com.igorwojda.cache.advancedlru
 
+import java.time.Clock
+import java.time.Duration
 import java.util.*
 
-// Implementation is using  combination of HashMap and LinkedList.
-// Time Complexity: O(1)
-private object Solution1 {
-    class AdvancedLRUCache(private val capacity: Int) {
-        private val map: MutableMap<String, CacheItem> = mutableMapOf()
-        private val priorityQueue: PriorityQueue<CacheItem> = PriorityQueue()
+// Implementation is using  combination of HashMap and PriorityQueue.
+// Time Complexity: O(N) (JVM priority queue is O(log(n)) on offer/poll methods and O(N) on remove(item) method)
+internal object Solution1 {
+    class AdvancedLRUCache<T: Any>(private val capacity: Int, private val clock: Clock = Clock.systemDefaultZone()) : LRUCache<T> {
+        private val map: MutableMap<String, CacheItem<T>> = mutableMapOf()
 
-        fun put(key: String, value: Int, priority: Int, expiryTime: Long) {
-            if (map.containsKey(key)) {
-                remove(key)
-            }
-
-            if (map.size == capacity) {
-                clearCache()
-            }
-
-            val item = CacheItem(key, value, priority, expiryTime)
-            map[key] = item
-            priorityQueue.add(item)
+        private val expiryQueue: PriorityQueue<CacheItem<T>> = PriorityQueue { item1, item2 ->
+            compareBy<CacheItem<T>>({ it.expiryTime }).compare(item1, item2)
         }
 
-        fun get(key: String): Int? {
+        private val priorityQueue: PriorityQueue<CacheItem<T>> = PriorityQueue { item1, item2 ->
+            compareBy<CacheItem<T>>({ it.priority }, { it.lastUsed }).compare(item1, item2)
+        }
+
+        override fun put(key: String, value: T, priority: Int, ttl: Duration) {
+            remove(key)
+            checkAndExpireCachedItems()
+
+            val item = CacheItem(key, value, priority, clock.millis() + ttl.toMillis(), clock.millis())
+            map[key] = item
+
+            expiryQueue.offer(item)
+            priorityQueue.offer(item)
+        }
+
+        override fun get(key: String): T? {
             val item = map[key]
 
-            return if (item == null || item.expiryTime < getSystemTimeForExpiry()) {
+            return if (item == null) {
+                null
+            } else if (item.expiryTime < clock.millis()) {
+                expiryQueue.remove(item)
+                priorityQueue.remove(item)
                 null
             } else {
-                item.lastUsed = System.currentTimeMillis()
+                priorityQueue.remove(item)
+                priorityQueue.add(item.touch(clock.millis()))
                 item.value
             }
         }
@@ -38,46 +49,61 @@ private object Solution1 {
             val item = map[key]
 
             item?.let {
-                it.expiryTime = 0L // Mark as expired for next eviction
+                expiryQueue.remove(item)
+                priorityQueue.remove(item)
                 map.remove(key)
             }
         }
 
-        private fun clearCache() {
-            while (priorityQueue.isNotEmpty() && priorityQueue.peek().expiryTime < getSystemTimeForExpiry()) {
-                val item = priorityQueue.poll()
+        private fun checkAndExpireCachedItems() {
+            if (map.size < capacity) return
 
-                if (map.containsKey(item.key) && map[item.key] == item) {
-                    map.remove(item.key)
-                }
+            while (expiryQueue.isNotEmpty() && expiryQueue.peek().expiryTime < clock.millis()) {
+                val item = expiryQueue.poll()
+                map.remove(item.key)
+                priorityQueue.remove(item)
             }
+
+            if (map.size < capacity) return
 
             if (priorityQueue.isEmpty()) return
 
             val item = priorityQueue.poll()
-
-            if (map.containsKey(item.key) && map[item.key] == item) {
-                map.remove(item.key)
-            }
+            map.remove(item.key)
+            expiryQueue.remove(item)
         }
 
-        private data class CacheItem(
+        private class CacheItem<T>(
             val key: String,
-            var value: Int,
-            var priority: Int,
-            var expiryTime: Long,
-        ) : Comparable<CacheItem> {
-            var lastUsed: Long = System.currentTimeMillis()
+            val value: T,
+            val priority: Int,
+            val expiryTime: Long,
+            val lastUsed: Long
+        ) {
+            // only compare equality by `key`
+            override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (other !is CacheItem<*>) return false
+                if (key == other.key) return true
+                return false
+            }
 
-            override fun compareTo(other: CacheItem) = when {
-                expiryTime != other.expiryTime -> expiryTime.compareTo(other.expiryTime)
-                priority != other.priority -> priority.compareTo(other.priority)
-                else -> lastUsed.compareTo(other.lastUsed)
+            override fun hashCode(): Int {
+                return key.hashCode()
+            }
+
+            fun touch(
+                lastUsed: Long = this.lastUsed
+            ) = CacheItem(key, value, priority, expiryTime, lastUsed)
+
+            override fun toString(): String {
+                return "CacheItem(key='$key', value=$value, priority=$priority, expiryTime=$expiryTime, lastUsed=$lastUsed)"
             }
         }
 
-        // Returns fixed system time in milliseconds
-        private fun getSystemTimeForExpiry() = 1000
+        override fun toString(): String {
+            return "AdvancedLRUCache(capacity=$capacity, clock=$clock, map=$map, priorityQueue=$expiryQueue)"
+        }
     }
 }
 
