a255c37627e72a72b70ec0647aec78476a02de9c
Jayson Minard <jayson.minard@gmail.com>
2023-07-30 15:53:30 -0600

    enhancements for AnyCallback

--- src/test/kotlin/com/igorwojda/common/anycallback/Solution.kt
+++ src/test/kotlin/com/igorwojda/common/anycallback/Solution.kt
@@ -1,12 +1,41 @@
 package com.igorwojda.common.anycallback
 
-private object Solution1 {
-    private fun anyCallback(list: List<Int>, callback: (Int) -> Boolean): Boolean {
+internal object Solution1 {
+    fun anyCallback(list: List<Int>, callback: (Int) -> Boolean): Boolean {
         if (list.size == 1) {
             return callback(list.first())
         }
 
         return callback(list.first()) || anyCallback(list.drop(1), callback)
+    }
+}
+
+internal object Solution2 {
+    fun anyCallback(list: List<Int>, callback: (Int) -> Boolean): Boolean {
+        if (list.isEmpty()) return false
+        return callback(list.first()) || anyCallback(list.subList(1, list.size), callback)
+    }
+}
+
+internal object Solution3 {
+    fun anyCallback(list: List<Int>, callback: (Int) -> Boolean): Boolean {
+        fun _randomAccessOptimized(list: List<Int>, callback: (Int) -> Boolean): Boolean {
+            if (list.isEmpty()) return false
+            return callback(list.first()) || _randomAccessOptimized(list.subList(1, list.size), callback)
+        }
+
+        fun _sequentialOptimized(list: List<Int>, callback: (Int) -> Boolean): Boolean {
+            if (list.isEmpty()) return false
+            return callback(list.first()) || _sequentialOptimized(list.drop(1), callback)
+        }
+
+
+        return if (list is RandomAccess) {
+           _randomAccessOptimized(list, callback)
+        }
+        else {
+            _sequentialOptimized(list, callback)
+        }
     }
 }
 
