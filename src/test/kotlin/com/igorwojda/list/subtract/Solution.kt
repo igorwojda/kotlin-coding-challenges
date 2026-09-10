8509755bf3e9df18de794f747b063d978cac63f5
Igor Wojda <igor.wojda@gmail.com>
2026-09-10 16:52:34 +0200

    Upd solution

--- src/test/kotlin/com/igorwojda/list/subtract/Solution.kt
+++ src/test/kotlin/com/igorwojda/list/subtract/Solution.kt
@@ -1,19 +1,17 @@
 package com.igorwojda.list.subtract
 
 private object Solution1 {
-    private fun getSubtraction(
-        list1: List<String>,
-        list2: List<String>,
-    ): List<String> {
-        val frequencyMap1 = list1.groupingBy { it }.eachCount()
-        val frequencyMap2 = list2.groupingBy { it }.eachCount()
-
-        val itemList1 = frequencyMap1.flatMap { entry -> List(entry.value) { entry.key } }.toMutableList()
-        val itemList2 = frequencyMap2.flatMap { entry -> List(entry.value) { entry.key } }
-
-        itemList2.forEach { itemList1.remove(it) }
-
-        return itemList1
+    fun getSubtraction(list1: List<String>, list2: List<String>): List<String> {
+        val counts = list2.groupingBy { it }.eachCount().toMutableMap()
+        return list1.filter { item ->
+            val remaining = counts[item] ?: 0
+            if (remaining > 0) {
+                counts[item] = remaining - 1
+                false
+            } else {
+                true
+            }
+        }
     }
 }
 
